be08a05f54aad08c14c1b08398d3d09488a66c70
TishnuThankappan <official.tishnu@gmail.com>
2025-06-24 18:04:03 +0530

    gradle

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,11 +8,13 @@ plugins {
 android {
     namespace = "com.example.stibe_partner"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
+        // Enable core library desugaring for local notifications
+        isCoreLibraryDesugaringEnabled = true
     }
 
     kotlinOptions {
@@ -41,4 +43,8 @@ android {
 
 flutter {
     source = "../.."
+}
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.3")
 }
